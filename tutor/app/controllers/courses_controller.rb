41eca05dd44b7ecbb5793e2e7da8d73e52098cf8
Ahmed Osam <osam@osam-HP-ProBook-450-G1.(none)>
2014-03-27 20:26:39 +0200

    add

--- tutor/app/controllers/courses_controller.rb
+++ tutor/app/controllers/courses_controller.rb
@@ -1,44 +1,7 @@
 class CoursesController < ApplicationController
-  
-  def destroy
-  	Course.last.destroy
-    flash[:success] = "Course deleted."
-    redirect_to :action => 'show'
-  end
+	def show
 
-  def new
-    if(@t == nil)
-     @t = Course.new
-   end
-  end
+			@courses = Lecturer.find_by_name('Lecturer2').courses
+		end
+	end
 
- def create
-  @t  = Course.new
-  @t.name = course_params[:name]
-  @t.code = course_params[:code]
-  @t.year = course_params[:year]
-  @t.semester = course_params[:semester]
-  @t.description = course_params[:description]
-  
-  if @t.save
-render text: "Course Successfully Created "
-#@t.discussion_board = Discussion_board.new
-
-  # redirect_to('show')
-  else render :action=>'new'  
-      
-  end
-
-
-  def show
-  	render text: "Course Added Successfully"
-  end
-
-  def manage
-  end
-
-  private def course_params 
-	params.require(:course).permit(:name,:code,:year,:semester,:description)
-  end
-
-end
--- tutor/app/models/topic.rb
+++ tutor/app/models/topic.rb
@@ -1,14 +1,16 @@
 class Topic < ActiveRecord::Base
 	
 	#Validations
-	validates :title, :description, presence: true
 
 	#Relations
 	has_many :tracks, dependent: :destroy
+
 	belongs_to :course
 	belongs_to :owner, class_name: "Stuff", foreign_key: :stuff_id
-	#Scoops
 
+	#Scoops
 	#Methods
-	
+
+
+
 end
